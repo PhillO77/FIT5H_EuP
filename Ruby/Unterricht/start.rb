4020ff952a2e3cad926cddddc6282ca3b78f4673
PhillO77 <Phillo77@hotmail.de>
2016-09-20 12:53:19 +0200

    Aktuelles

--- Ruby/Unterricht/start.rb
+++ Ruby/Unterricht/start.rb
@@ -1,40 +1,11 @@
-#Erzeugen Sie folgende Ausgabe:
+def movie_listing(movie, rank)
 
+	
+	"Movie: #{movie} ist ein guter Film, mit der Punktzahl: #{rank} am #{set_time}"
+end
 
-#Larry has a health of 60.
-#CURLY has a health of 125.
-#*************Moe has a health of 100.*************
-#*************Moe has a health of 100.*************
-#Shemp......................... 90 health
-#Shemp......................... 90 health
-#Players:
-#        Larry
-#        Curly
-#        Moe
-#The game started on Thursday 08/02/2012 at 11:42AM
+def set_time
+	zeit = Time.now.strftime("%d.%m.%Y")
+end
 
-
-
-
-players1 = "Larry"
-players2 = "Curly"
-players3 = "Moe"
-players4 = "Shemp"
-zahl1 = "100"
-tag = Time.new().strftime("%A %C/%m/%Y")
-zeit = Time.new().strftime("%H:%M")
-
-allenamen = <<HEREDOC
-#{players1} has a health of 60.
-#{players2.upcase} has a health of 125.
-#{players3.rjust(15, '*')} has a health of #{zahl1.ljust(15, '*')}
-#{players3.rjust(15, '*')} has a health of #{zahl1.ljust(15, '*')}
-#{players4.ljust(25, '.')} 90 health
-#{players4.ljust(25, '.')} 90 health
-Players:
-	#{players1}
-	#{players2}
-	#{players3}
-The game started on #{tag} at #{zeit}
-HEREDOC
-puts allenamen
+puts movie_listing(ARGV[0], 10)
--- Ruby/Unterricht/studio_game.rb
+++ Ruby/Unterricht/studio_game.rb
@@ -1,26 +1,5 @@
-name1 = "larry"
+def movie_listing
+	"Movie: Tschick ist ein guter Film."
+end
 
-health = 60
-
-puts "#{name1}'s health is#{health}"
-puts name1 + "'s health is" #{health}"
-
-puts "#{name1}'s health is#{3 * health}"
-
-puts "#{name1}'s health is#{health / 9}"
-puts "#{name1}'s health is #{ health / 9. to_i} i=integer
-puts "#{name1}'s health is #{ health / 9. to_f} f=float
-
-name1 = "Larry"
-name2 = "Curly"
-name3 = "Moe"
-
-puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
-
-allenamen = <<HEREDOC
-Players:
-	#{name1}
-	#{name2}
-	#{name3}
-HEREDOC
-puts allenamen
+puts movie_listings
